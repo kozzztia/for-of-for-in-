c6d9bb6e95f7408a6dffc6a168a9410b70ea8c62
Kozzztia <kozzztia@ukr.net>
2021-12-26 22:33:39 +0200

    last task from pre unit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,57 @@
-$(document).ready(function(){
+let push = document.querySelector('.push')
+let div = document.querySelector('.div')
+let input = document.querySelector('.input')
+let p = document.querySelector('.p')
 
 
+// push.onclick = () => {
+//   let text = input.value;
+//   let string = text.split(" ").join("");
+//   console.log(string)
+//   let arr = string.split("");
+//   console.log(arr)
+//   let set = new Set(arr);
+//   console.log(set)
+//   let obj = {};
+//   for (let i of set) {
+//     let count = 0;
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] == i) {
+//         count++;
+//       }
+//     }
+//     obj[i] = count;
+//   }
+//   console.log(obj);
+// };
 
+push.onclick = function () {
+  let text = input.value;
+  console.log(text);
+  // ---------------------------------find text
+  let textArr = [];
+  for (let i = 0; i < text.length; i++) {
+    textArr.push(text.charAt(i));
+  }
+  console.log(textArr);
+  // ----------------------- create new array
+  let textSet = new Set(textArr);
+  console.log(textSet);
+  // ------------------------create set
+  let textObj = {}
+  // ----------------------create object
 
+  for (let key of textSet) {
+    count = 0;
+    // -----------------------create count of elements
+    for (let i = 0; i < textArr.length; i++) {
+      if (textArr[i] === key) {
+        //  if word of array === word in set
+        count++;
+      }
+    }
+    textObj[key] = count;
+  }
+  console.log(textObj);
+}
 
-
-
-
-  });
